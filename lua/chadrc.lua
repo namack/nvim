24d62649fcb1245276ad283a6640e17c26a31ed4
Nate Amack <nate@amack.net>
2024-05-01 14:13:41 -0600

    update colors for yaml

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -11,6 +11,7 @@ M.ui = {
     ["@variable"] = { fg = "#01a0e4" },
     ["@variable.member"] = { fg = "#f8f8f0" },
     ["@variable.builtin"] = { fg = "#21cbdf" },
+    ["@function"] = { fg = "#01a0e4" },
     ["@function.call"] = { fg = "#00fb00" },
     ["@function.method.call"] = { fg = "#00fb00" },
     ["@punctuation.delimiter"] = { fg = "#f8f8f0" },
@@ -20,11 +21,14 @@ M.ui = {
     ["@number"] = { fg = "#ff8fd8" },
     ["@operator"] = { fg = "#ff4050" },
     ["@constant.builtin"] = { fg = "#7759ff" },
-    ["@type.builtin"] = { fg = "#01a0e4" },
+    ["@type.builtin"] = { fg = "#21cbdf" },
     ["@punctuation.bracket"] = { fg = "#5fb0fc" },
     ["@string"] = { fg = "#ffd945" },
+    ["@tag"] = { fg = "#ff4050" },
+    ["@tag.delimiter"] = { fg = "#f8f8f0" },
     ["@keyword.operator"] = { fg = "#ff4050" },
     ["@keyword.return"] = { fg = "#ff4050" },
+    ["@keyword.conditional"] = { fg = "#ff4050" },
   },
 
   hl_add = {
@@ -32,6 +36,10 @@ M.ui = {
     ["@keyword.coroutine"] = { italic = true, fg = "#ff4050" },
     ["@keyword.import.typescript"] = { fg = "#ff4050" },
     ["@keyword.export"] = { fg = "#ff4050" },
+    yamlBlockMappingKey = { fg = "#00fb87" },
+    yamlFlowString = { fg = "#fffa00" },
+    yamlFlowStringDelimiter = { fg = "#fffa00" },
+    yamlPlainScalar = { fg = "#21cbdf" },
   },
 }
 
